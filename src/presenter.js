1e7edcb73eff464f2a60ce13b9563aa22c1b6034
Mateo <mateo>
2023-02-15 19:16:37 -0800

    cecrce

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,16 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
+
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+
+const third = document.querySelector("#tercer-numero");
+const fourth = document.querySelector("#cuarto-numero");
+const formMultiplicar = document.querySelector("#multiplicar-form");
+const divMultiplicar = document.querySelector("#resultadoMultiplicador-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +19,12 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+  formMultiplicar.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    const tercerNumber = Number.parseInt(third.value);
+    const cuartoNumber = Number.parseInt(fourth.value);
+  
+    divMultiplicar.innerHTML = "<p>" + multiplicar(tercerNumber, cuartoNumber) + "</p>";
 });
